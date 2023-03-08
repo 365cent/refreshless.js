b14cd548c19ccad0bac139f800dd19c51a7ee4e7
365cent <74791570+365cent@users.noreply.github.com>
2023-03-08 13:24:04 -0500

    Update refreshless.js

--- refreshless.js
+++ refreshless.js
@@ -1,18 +1,18 @@
 const cache = {};
 
 document.addEventListener("click", async function (event) {
-  if (event.target.tagName === "A") {
-    event.preventDefault();
-    const page = event.target.href;
-    let content = cache[page];
-    if (!content) {
-      const response = await fetch(page);
-      content = await response.text();
-      cache[page] = content;
+    if (event.target.tagName === "A") {
+        event.preventDefault();
+        const page = event.target.href;
+        let content = cache[page];
+        if (!content) {
+            const response = await fetch(page);
+            content = await response.text();
+            cache[page] = content;
+        }
+        document.body.innerHTML = content;
+        window.history.pushState(page, null, page);
     }
-    document.body.innerHTML = content;
-    window.history.pushState(page, null, page);
-  }
 });
 
 addEventListener("popstate", (event) => {
